7991e2746b84d324abe60a5196ec94d51b7b01f8
Döme Lörinczy <dome.lorinczy@gmail.com>
2022-01-04 14:25:07 +0100

    separated the validate functions

--- misc/misc.py
+++ misc/misc.py
@@ -221,9 +221,137 @@ def validate(model: torch.nn.Module, loader: DataLoader, loss_fn, num_classes):
     return dice, ce, mad
 
 
+def normalize(x, y, x_mean, x_std, y_mean, y_std):
+  x_mean = torch.tile(x_mean, (x.shape[0], x.shape[1], 1))
+  x_std = torch.tile(x_std, (x.shape[0], x.shape[1], 1))
+  y_mean = torch.tile(y_mean, (x.shape[0], x.shape[1], 1))
+  y_std = torch.tile(y_std, (x.shape[0], x.shape[1], 1))
+  x_normalized = (x - x_mean) / x_std
+  y_normalized = (y - y_mean) / y_std
+  return x_normalized, y_normalized
+
+@torch.no_grad()
+def denormalize(y, y_mean, y_std):
+  y_mean = torch.tile(y_mean, (y.shape[0], y.shape[1], 1))
+  y_std = torch.tile(y_std, (y.shape[0], y.shape[1], 1))
+  y = (y * y_std.to(y.device) + y_mean.to(y.device))
+  return y
+
+
+@torch.no_grad()
+def validate_retlstm(model: torch.nn.Module, loader: DataLoader, mean_std, num_classes):
+    """
+    Validates the model.
+    :param model: torch.nn.Module
+    :param loader: torch.utils.data.DataLoader
+    :param loss_fn: Function
+    :return: torch.Tensor, torch.Tensor
+    """
+    model.eval()
+    loader = tqdm(loader, desc="Validation", leave=False)
+    device = next(model.parameters()).device
+    ce = Metric(0, num_classes - 2)
+    mad = Metric(0, num_classes - 3)
+    dice = Metric(0, num_classes - 1)
+    for batch_idx, (data, target) in enumerate(loader):
+        data, _ = normalize(data, target, *mean_std)
+        data, target = data.swapaxes(0, 1).to(device), target.swapaxes(0, 1).to(device)
+        prediction = model(data)
+        prediction = denormalize(prediction, *mean_std[2:])
+        mse = ((target - prediction)**2).sum(1).mean(0)
+        ce_avg = ce.update({idx: round(value, 3) for idx, value in enumerate(mse.tolist())}, return_avg=True)
+        mad_dict = {i: (prediction[:, :, i] - target[:, :, i]).abs().mean().item() for i in range(num_classes - 2)}
+        mad_avg = mad.update(mad_dict, return_avg=True)
+        target = get_layer_mask_from_boundaries(target.swapaxes(0, 1), data.shape[-1]).long()
+        prediction_mask = get_layer_mask_from_boundaries(prediction.swapaxes(0, 1), data.shape[-1]).long()
+        layer_mask = get_layer_channels(prediction_mask, num_classes)
+        dice_avg = dice.update(dice_acc(layer_mask, target, num_classes), return_avg=True)
+        loader.set_postfix(dice=dice_avg, mad=mad_avg, ce=ce_avg)
+    model.train()
+    dice.normalize()
+    ce.normalize()
+    mad.normalize()
+    return dice, ce, mad
+
+
 def get_layer_mask_from_boundaries(y, a_scan_length=496):
-    idx_tensor = torch.tile(torch.arange(0, a_scan_length), dims=(*y.shape[:-1], 1))
+    idx_tensor = torch.tile(torch.arange(0, a_scan_length, device=y.device), dims=(*y.shape[:-1], 1))
     out = torch.zeros_like(idx_tensor)
     for idx, row in enumerate(y.permute((-1, 0, 1))):
         out[idx_tensor > row.unsqueeze(-1)] = idx + 1
     return out
+
+
+def show_layers_from_boundary_array(img_array, layer_array, fluid=None):
+    err_msg = "layer boundaries not compatible with image width"
+    assert img_array.shape[1] == layer_array.shape[0], err_msg
+    dme_colorcode = {
+        1: (170, 160, 250),
+        2: (120, 200, 250),
+        3: (80, 200, 250),
+        4: (50, 230, 250),
+        5: (20, 230, 250),
+        6: (0, 230, 250),
+        7: (0, 230, 100),
+        8: (180, 255, 255)  # fluid
+    }
+    amd_colorcode = {
+        1: (180, 200, 250),
+        2: (120, 200, 250),
+    }
+    zeros = np.zeros_like(img_array, dtype="uint8")
+    hue = np.zeros_like(img_array, dtype="uint8")
+    saturation = np.zeros_like(img_array, dtype="uint8")
+    value = np.zeros_like(img_array, dtype="uint8")
+    mask = np.zeros(img_array.shape, dtype="uint8")
+    for w in range(img_array.shape[1]):
+        if ~np.isnan(layer_array[w, :]).any():
+            last_boundary = int(layer_array[w, 0])
+            for idx, h in enumerate(layer_array[w, 1:]):
+                curr_boundary = int(h) + 1
+                mask[last_boundary:curr_boundary, w] = idx + 1
+                last_boundary = curr_boundary
+    if fluid is not None:
+        mask[fluid != 0] = 8
+    if layer_array.shape[1] == 8:
+        for klass, hsv in dme_colorcode.items():
+            hue[mask == klass] = hsv[0]
+            saturation[mask == klass] = hsv[1]
+            value[mask == klass] = hsv[2]
+    if layer_array.shape[1] == 3:
+        for klass, hsv in amd_colorcode.items():
+            hue[mask == klass] = hsv[0]
+            saturation[mask == klass] = hsv[1]
+            value[mask == klass] = hsv[2]
+    alpha = np.zeros_like(img_array, dtype="uint8")
+    alpha[mask != 0] = 255
+    img_stack = np.array([zeros, zeros, img_array]).transpose((1, 2, 0))
+    img = Image.fromarray(img_stack, mode="HSV")
+    colored_mask_stack = np.array([hue, saturation, value]).transpose((1, 2, 0))
+    colored_mask = Image.fromarray(colored_mask_stack, mode="HSV")
+    alphaimg = Image.fromarray(alpha)
+    img_with_boundaries = Image.composite(colored_mask, img, alphaimg)
+    return img_with_boundaries
+
+
+def get_mean_std(loader):
+  x, y = next(iter(loader))
+  x_mean = torch.zeros((x.shape[-1],))
+  x_square_mean = torch.zeros((x.shape[-1],))
+  y_mean = torch.zeros((y.shape[-1],))
+  y_square_mean = torch.zeros((y.shape[-1],))
+  num_a_scans = 0
+  for x, y in loader:
+    x_mean += x.sum((0, 1))
+    x_square_mean += (x**2).sum((0, 1))
+    y_mean += y.sum((0, 1))
+    y_square_mean += (y**2).sum((0, 1))
+    num_a_scans += x.shape[1] * x.shape[0]
+
+  x_mean /= num_a_scans
+  x_square_mean /= num_a_scans
+  y_mean /= num_a_scans
+  y_square_mean /= num_a_scans
+  x_std = (x_square_mean - x_mean**2).abs().sqrt()
+  y_std = (y_square_mean - y_mean**2).abs().sqrt()
+  return x_mean, x_std, y_mean, y_std
