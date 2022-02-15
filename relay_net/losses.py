adjusting metric to fluid training, fixing relaynet loss (correct weighting)

relay_net/losses.py
@@ -8,7 +8,7 @@ class DiceLoss(nn.Module):
 
     def __init__(self, weight_channel, num_classes):
         super(DiceLoss, self).__init__()
-        self.weight_channel = weight_channel / weight_channel.sum()
+        self.weight_channel = weight_channel
         self.num_classes = num_classes
 
     def forward(self, predictions: torch.Tensor, targets: torch.Tensor):
@@ -24,7 +24,9 @@ class DiceLoss(nn.Module):
         """
         predictions_exp = predictions.exp()
         dice_coeff = dice_coefficient(predictions_exp, targets, self.num_classes).mean(0)
-        loss = (self.weight_channel * (1 - dice_coeff)).mean(0)
+        weight = 1 + self.weight_channel
+        weight /= weight.sum()
+        loss = (weight * (1 - dice_coeff)).sum()
         return loss
 
 
@@ -46,14 +48,14 @@ class CombinedLoss(nn.Module):
 
     def forward(self, predictions, targets):
         weight_boundary = F.pad(targets[:, 1:, :] - targets[:, :-1, :], (0, 0, 0, 1), "constant", 0)
-        weight_boundary = weight * self.weight_boundary_cross
-        weight_channel = torch.zeros(*targets.shape, self.num_classes).scatter_(
+        weight_boundary = weight_boundary * self.weight_boundary_cross
+        weight_channel = torch.zeros(*targets.shape, self.num_classes, device=targets.device).scatter_(
                   -1, targets.unsqueeze(-1), 1
         )
         weight_channel = weight_channel * self.weight_channel_cross
         weight_channel = weight_channel.max(-1).values
-        cross_entropy_loss = (self.cross_entropy_loss_fn(predictions, targets) * (
-                  1 + weight_boundary + weight_channel
-        )).mean()
+        weight = 1 + weight_boundary + weight_channel
+        weight /= weight.sum()
+        cross_entropy_loss = (self.cross_entropy_loss_fn(predictions, targets) * weight).sum()
         dice_loss = self.dice_loss_fn(predictions, targets)
         return cross_entropy_loss, dice_loss

utils/metric.py
@@ -3,11 +3,10 @@ from utils.misc import dice_coefficient, get_layer_channels
 
 
 def contour_error(pred, target):
-    # classes = sorted(list(set(target.flatten().tolist())))
     classes = range(1, 9)
     ce_dict = {}
     mse_fn = nn.MSELoss()
-    for klass in classes[1:]:
+    for klass in classes:
         pred_mask = (pred == klass).int()
         pred_diff = (pred_mask[:, 1:] - pred_mask[:, :-1])
         pred_idx = pred_diff.max(1).indices
@@ -15,15 +14,14 @@ def contour_error(pred, target):
         target_diff = target_mask[:, 1:] - target_mask[:, :-1]
         target_idx = target_diff.max(1).indices
         mse = mse_fn(pred_idx.float(), target_idx.float())
-        ce_dict[klass - 1] = mse.sqrt().item()
+        ce_dict[klass] = mse.sqrt().item()
     return ce_dict
 
 
 def mad_lt(pred, target):
-    # classes = sorted(list(set(target.flatten().tolist())))
     classes = range(1, 8)
     mad_dict = {}
-    for klass in classes[1:-1]:
+    for klass in classes:
         pred_mask = (pred == klass).int()
         pred_diff = pred_mask[:, 1:] - pred_mask[:, :-1]
         pred_widths = pred_diff.shape[1] - 1 - pred_diff.flip(1).min(1).indices - pred_diff.max(1).indices
@@ -31,7 +29,7 @@ def mad_lt(pred, target):
         target_diff = target_mask[:, 1:] - target_mask[:, :-1]
         target_widths = target_diff.shape[1] - 1 - target_diff.flip(1).min(1).indices - target_diff.max(1).indices
         mad = (pred_widths - target_widths).abs().float().mean()
-        mad_dict[klass - 1] = mad.item()
+        mad_dict[klass] = mad.item()
     return mad_dict
 
 
@@ -39,18 +37,6 @@ def dice_acc(pred, target, num_classes):
     dice = dice_coefficient(pred, target, num_classes).mean(0)
     dice_dict = {klass: dice[klass].item() for klass in range(dice.shape[0])}
     return dice_dict
-
-
-# This initialization consistenly underperforms the default Glorot initialization
-def initialize(model):
-  for m in model.modules():
-    if isinstance(m, nn.Conv2d):
-      nn.init.kaiming_normal_(m.weight, nonlinearity="relu")
-      # nn.init.xavier_normal_(m.weight)
-    if isinstance(m, nn.Linear):
-      # nn.init.xavier_normal_(m.weight)
-      nn.init.kaiming_normal_(m.weight, nonlinearity="relu")
-      nn.init.constant_(m.bias, 0)
 
 
 def intersection_over_union(prediction, target, num_classes):

utils/show.py
@@ -43,11 +43,15 @@ def show_layers_from_mask(img, mask, normed=False):
 
 def show_layers_from_boundary(img_array, layer_array, mean_std=None, a_scan_length=496, fluid=None, normed=False):
     err_msg = "layer boundaries not compatible with image width"
+    if len(img_array.shape) == 3:
+        img_array.squeeze_()
+    if len(layer_array.shape) == 3:
+        layer_array.squeeze_()
     assert img_array.shape[1] == layer_array.shape[1], err_msg
     if type(img_array) == torch.Tensor:
-        img_array = img_array.numpy()
+        img_array = img_array.detach().cpu().numpy()
     if type(layer_array) == torch.Tensor:
-        layer_array = layer_array.numpy()
+        layer_array = layer_array.detach().cpu().numpy()
     if mean_std:
         img_array = img_array * mean_std[1].numpy() + mean_std[0].numpy()
         layer_array = layer_array * mean_std[3].numpy() + mean_std[2].numpy()
@@ -105,14 +109,19 @@ def show_layers_from_boundary(img_array, layer_array, mean_std=None, a_scan_leng
 
 
 @torch.no_grad()
-def show_prediction(model, data, target, colab=True):
+def show_prediction(model, data, target, mean_std, standardized=False, colab=True):
     if len(data.shape) == 2:
         data.unsqueeze_(0).unsqueeze_(0)
-    model = model.cpu()
+    dev = next(model.parameters()).device
+    model.cpu()
+    if not standardized:
+      data = (data - mean_std[0]) / mean_std[1]
     prediction = model(data.float())
     prediction = prediction.max(1).indices
-    img_pred = show_layers_from_mask((data.squeeze() * 255).byte(), prediction.squeeze())
-    img_target = show_layers_from_mask((data.squeeze() * 255).byte(), target.squeeze())
+    data = (data.squeeze() * mean_std[1] + mean_std[0]) * 255
+    img_pred = show_layers_from_mask(data.byte(), prediction.squeeze())
+    img_target = show_layers_from_mask(data.byte(), target.squeeze())
+    model.to(dev)
     if colab:
         return img_pred, img_target
     else:

utils/validate.py
@@ -22,8 +22,8 @@ def validate_relaynet(model: torch.nn.Module, loader: DataLoader, loss_fn, num_c
     device = next(model.parameters()).device
     cross_losses = []
     dice_losses = []
-    ce = Metric(0, num_classes - 2)
-    mad = Metric(0, num_classes - 3)
+    ce = Metric(1, 8)
+    mad = Metric(1, 7)
     dice = Metric(0, num_classes - 1)
     for batch_idx, (data, target) in enumerate(loader):
         data = data.unsqueeze(1).float().to(device)